8c8259386343218eb71117071f308db51fe2ec03
Sakthi priyan <sakthipriyan.m99@gmail.com>
2024-04-23 11:19:23 +0530

    Ui part

---
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -0,0 +1,39 @@
+import Messages from "./Messages";
+import MessageInput from "./MessageInput";
+import {TiMessages} from "react-icons/ti"
+const MessageContainer = () => {
+    const noChatSelected = false;
+  return (
+    <div className="md:min-w-[450px] flex flex-col">
+        {noChatSelected ? (
+            <NoChatSelected />
+        ):(
+            <>
+            <div className="bg-slate-500 px-4 py-2 mb-2">
+              <span className="label-text">To:</span>
+              {""}
+              <spam className="text-gray-900 font-b">Sakthi</spam>
+            </div>
+    
+            <Messages />
+            <MessageInput />
+          </>
+        )}
+
+    </div>
+  );
+};
+
+export default MessageContainer;
+
+const NoChatSelected = () => {
+    return(
+        <div className="flex items-center justify-center w-full h-full">
+            <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
+                <p>Welcome 👋 Priyan ❄</p>
+                <p>Select a chat to start messaging</p>
+                <TiMessages className='text-3xl md:text-6xl text-center' />
+            </div>
+        </div>
+    )
+}
